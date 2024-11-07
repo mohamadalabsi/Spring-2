3551d38e4f74a89cde1b8bbc180bb34d26af5470
mohamadalabsi <absimoda1@gmail.com>
2024-11-07 23:30:56 +0100

    Setting up the classes

---
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+}
