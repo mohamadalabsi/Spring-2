184200f0c1d28e604a1dc0835142a306941b1295
mohamadalabsi <absimoda1@gmail.com>
2024-11-15 23:39:35 +0100

    some updates and features i applied on student class

--- demo/src/main/java/com/example/demo/model/Student.java
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.sound.midi.Sequence;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Date;
@@ -16,13 +17,28 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
+@Table (name = "student_table",
+      uniqueConstraints = {
+         @UniqueConstraint(name = "student_email", columnNames = "email")
+      })
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) this or down
+//    now this to specify the name of the column in the database and some other things
+    @SequenceGenerator( name = "student_sequence",
+                        sequenceName = "student_sequence",
+                         allocationSize = 1)
+    @GeneratedValue(strategy =  GenerationType.SEQUENCE , generator = "student_sequence")
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
+    @Column(name = "name", nullable = false, columnDefinition = "Text")
     private String name;
+    @Column(name = "age", nullable = false)
     private int age;
+    @Column(name = "email", nullable = false, columnDefinition = "Text"
+//            , unique = true this should not be written here if you want to change the name , but if it does not matter u should write it 
+    )
     private String email;
 //    @Transient  so this does that birthdate will not be a column in ur database , but by get method it will be included because in repo we gave it this class name and it will get all the properties from it also if it is not in the db
 //    private Date birthdate;
